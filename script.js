1d4499388c02cea08efd72ec35720ed231e9ba0f
Apoorva Kishor Rupnarayan <166980134+Apo-orva17@users.noreply.github.com>
2025-02-17 16:01:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+//function to add task
+let input=document.getElementById("task");
+let box=document.getElementById("list");
+
+function addele(){
+    if(input.value === ''){
+        alert("You must write something!!!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=input.value;
+        box.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="";
+        li.appendChild(span);
+    }
+    saveData()
+}
+
+
+//function to update when checked
+box.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()}
+    }, false);
+
+    //function to save task even when the site refreshes
+    function saveData(){
+        localStorage.setItem("data", box.innerHTML);
+    }
+
+    //function to display saved task
+    function displayData(){
+        box.innerHTML=localStorage.getItem("data");
+    }
+    displayData();
